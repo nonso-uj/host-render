5a10437b4dbdcd028f16bc53b33915f04ebede4b
nonso_uj <nonso.udonne@gmail.com>
2024-11-18 12:25:04 +0100

    db linksss

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$4m8)c0sd9i#m4s!72t*vlo88x5b^w2x@z_zza*$7$@w2p#xk#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['host-render-etmp.onrender.com']
 
 
 # Application definition
